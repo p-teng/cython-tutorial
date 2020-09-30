b7db4158376f5088e335c65c0f11ed2093416597
p-teng <54685205+p-teng@users.noreply.github.com>
2020-09-30 09:59:32 -0400

    Create malloc.pyx

---
+++ malloc.pyx
@@ -0,0 +1,28 @@
+# malloc, realloc, and free are still used in cython
+
+# example malloc usage:
+import random
+from libc.stdlib cimport malloc, free
+
+def random_noise(int number=1):
+    cdef int i
+    # allocate number * sizeof(double) bytes of memory
+    cdef double *my_array = <double *> malloc(number * sizeof(double))
+    if not my_array:
+        raise MemoryError()
+
+    try:
+        ran = random.normalvariate
+        for i in range(number):
+            my_array[i] = ran(0, 1)
+
+        # ... let's just assume we do some more heavy C calculations here to make up
+        # for the work that it takes to pack the C double values into Python float
+        # objects below, right after throwing away the existing objects above.
+
+        return [x for x in my_array[:number]]
+    finally:
+        # return the previously allocated memory to the system
+        free(my_array)
+        
+# Blocks of memory obtained with malloc or PyMem_Malloc() must be subsequently removed with free or PyMem_Free(). They must always use the matching type of free function.
